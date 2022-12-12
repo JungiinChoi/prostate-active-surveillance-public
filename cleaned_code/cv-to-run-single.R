8c7f0ff1e16076a149c2fe03b3a77611b5823447
Zitong Wang <zwang238@jh.edu>
2022-12-12 18:49:42 -0500

    update to-run-single.R

--- cleaned_code/cv-to-run-single.R
+++ cleaned_code/cv-to-run-single.R
@@ -14,35 +14,46 @@
 
 ### 1. Clear workspace
 rm(list=ls())
-mri_role <- "both" #moderator, both, outcome, 0
-
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+#mri_role <- "both" #moderator, both, outcome, 0
+mri_role <- args[3]
+to_mask <- as.numeric(args[2])
+K <- as.numeric(args[1])
+workdir <- args[4]
 ### 2. Define directories, file names
-#### These will have to be adjusted by the user
-base.location <- '/users/zwang3/PAS_clean/' 
+#workdir <- "/users/zwang3/PAS_11722"
+base.location <- workdir
+location.of.data <- paste0(base.location, "/data")
+location.of.r.scripts <- paste0(base.location, "/code")
+location.of.generated.files <- paste0(base.location, "/generated-files-sh")
+
+base.location <- "/Users/zitongwang/Downloads/prostate-active-surveillance-vDaan/"
 location.of.data <- paste0(base.location, "data")
-location.of.r.scripts <- paste0(base.location, "cleaned_code")
-location.of.generated.files <- paste0(base.location, "cleaned_code/generated-files-cv")
+location.of.r.scripts <- paste0(base.location, "code")
+location.of.generated.files <- paste0(base.location, "generated-files-sh")
+
 
 name.of.pt.data <- "Demographic_6.15.csv" #"demographics with physician info.2015.csv"
 name.of.bx.data <- "Biopsy_6.15.csv" #"Biopsy data_2015.csv"
 name.of.psa.data <- "PSA_6.15.csv" #"PSA.2015.csv"
 name.of.tx.data <- "Treatment_6.15.csv" #"treatment_2015.csv"
 name.of.mri.data <- "MRI_6.15.csv" 
+
 ### 3. Load libraries
-#### may need to go back and add command for automatic installation
-library("lme4")
-library("splines")
-library("gtools")
-library("bayesm")
-library("rjags")
-library("R2jags")
-library("RCurl")
+#### may need to go back and add command for automatic installation - YESS done Zitong
+# Package names
+packages <- c("lme4",  "dplyr", "tidyr", "tidyverse", "readr",
+              "splines", "gtools", "bayesm", "rjags", "R2jags", "RCurl")
 
-### 4. Set date of data pull
-#### This should be adjusted by the user
-#date.pull<-as.numeric(as.Date("1/9/18","%m/%d/%y"))
-date.pull<-as.numeric(Sys.Date())
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
 
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
 ### 5. Run R scripts!
 
 #Load data; tidy, check, and shape
@@ -50,36 +61,39 @@ data.check <- function(condition, message){
   if(condition==FALSE){print(paste(message, "Program terminated.", sep=" "))}
   stopifnot(condition)}
 
-#source(paste(location.of.r.scripts,"data-load-check-and-shaping.R",sep="/"))
-load(paste0(location.of.r.scripts, "/IOP-data-shaping-work-space.RData"))
+#Load data; tidy, check, and shape
+#source("model/data-load-check-and-shaping.R") #need to rerun this with new data
+load(paste(location.of.generated.files,"IOP-data-shaping-work-space-11.7.22-withMRI.RData",sep="/"))
 
 
-  to.mask<- to_mask
-  #Load tidied/shaped data; further shaping for JAGS
-  source(paste(location.of.r.scripts,"cv-data-prep-for-jags.R",sep="/"))
-  
-  #Load arguments for JAGS
-  source(paste(location.of.r.scripts,"cv-argument-prep-for-jags.R",sep="/"))
-  
-  #Define JAGS model
-  source(paste(location.of.r.scripts,"cv-JAGS-prediction-model.R",sep="/"))
-  
-  #run model and save results
+to.mask<- to_mask
+K<-K
+
+options(warn = 0)
+#Load tidied/shaped data; further shaping for JAGS
+source(paste(location.of.r.scripts,"data-prep-for-jags.R",sep="/"))
+
+#Load arguments for JAGS
+source(paste(location.of.r.scripts,"argument-prep-for-jags.R",sep="/"))
+
+#run model and save results
+if(K==1){
+  seed <- 2022
+}else{
   seed <- to.mask
-  set.seed(seed)
-  outj<-jags(jags_data, inits=inits, 
-             parameters.to.save=params,
-             model.file=paste(location.of.r.scripts, 
-                              paste0("cv-JAGS-prediction-model-",mri_role,".txt"), sep="/"),
-             n.thin=n.thin, n.chains=n.chains, n.burnin=n.burnin, n.iter=n.iter)
-  out<-outj$BUGSoutput
-  for(j in 1:length(out$sims.list)){
-  	write.csv(out$sims.list[[j]],
-  	          paste(location.of.generated.files, "/jags-prediction-", names(out$sims.list)[j],"-",seed,".csv",sep=""))
-  }
+}
 
+#Define JAGS model
+source(paste(location.of.r.scripts,"JAGS-prediction-model.R",sep="/"))
 
-
-
-
-
+set.seed(seed)
+outj<-jags(jags_data, inits=inits, 
+           parameters.to.save=params,
+           model.file=paste(location.of.r.scripts, 
+                            jags_file_name, sep="/"),
+           n.thin=n.thin, n.chains=n.chains, n.burnin=n.burnin, n.iter=n.iter)
+out<-outj$BUGSoutput
+for(j in 1:length(out$sims.list)){
+  write.csv(out$sims.list[[j]],
+            paste(location.of.generated.files, "/jags-prediction-", names(out$sims.list)[j],"-",mri_role,seed,".csv",sep=""))
+}
