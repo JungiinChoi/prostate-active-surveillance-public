89046281aabbafe54941b13efd4ad9c8e36e0ee7
Zitong Wang <zwang238@jh.edu>
2022-12-12 19:16:07 -0500

    update data prep for jhpce

--- cleaned_code/cv-data-prep-for-jags.R
+++ cleaned_code/cv-data-prep-for-jags.R
@@ -1,7 +1,7 @@
 ########### SEED/GROUP TO MASK IS EITHER SET HERE OR IN EXAMPLE-CV-TO-RUN.R
 #### This should be adjusted by the user depending on how seed, masking set 
 # SEED <- as.numeric(Sys.getenv("SGE_TASK_ID"))
-# # For GAP3 analysis, make this seed=1,...,K and define grouping variable with values 1,...,K 
+# # For GAP3 analysis, make this seed=1,...,K and define grouping variable with values 1,...,K
 # to.mask<- SEED
 ###########
 
@@ -23,19 +23,20 @@
 
 
 
-### 0 .Load libraries
+### 0 .Load libraries --------
 library("lme4")
 library("splines")
 library("readr")
 library("dplyr")
 library("tidyr")
 
-### 1. Format pt-level data for JAGS run 
+### 1. Format pt-level data for JAGS run ---------
 #number of patients
-(npat <- dim(pt.data)[1])
+npat <- dim(pt.data)[1]
 
 #list of known true states
 cancer_data_true <- pt.data$true.pgg[!is.na(pt.data$true.pgg)]
+
 #number with known true state
 npat_cancer_known <- length(cancer_data_true)
 
@@ -44,13 +45,18 @@ npat_cancer_known <- length(cancer_data_true)
 set.seed(44)
 my.sample <- sample(1:npat_cancer_known)
 
+if(K>1){
+  folds <- cut(seq(1, npat_cancer_known), breaks=K, labels=F)
+}else{
+  folds <- rep(1, npat_cancer_known)
+}
+
 n <- npat
 pt.data$cvgroup<-rep(0,n)
 
 #save number of patients with true state to-be-masked
-if (K > 1){
+if (K>1){
   for (group in 1:K){
-    folds <- cut(seq(1, npat_cancer_known), breaks=K, labels=F)
     pt.data$cvgroup[my.sample[folds==group]]<-group}
 }
 
@@ -86,19 +92,25 @@ for(i in 1:n){psa.data$subj2[psa.data$subj==i]<-pt.data$subj2[pt.data$subj==i]}
 bx.full$subj2<-vector(length=dim(bx.full)[1])
 for(i in 1:n){bx.full$subj2[bx.full$subj==i]<-pt.data$subj2[pt.data$subj==i]}
 
+if(K==1 & sum(bx.full$subj2 != bx.full$subj)>0){
+  warning(paste0(sum(bx.full$subj2 != bx.full$subj), " patients indexing (subj) incorrectly for non-cv run in bx.full, check script"))}
 
+if(K==1 & sum(psa.data$subj2 != psa.data$subj)>0){
+  warning(paste0(sum(psa.data$subj2 != psa.data$subj), " patients indexing (subj) incorrectly for non-cv run in psa.data, check script"))}
+
+
+#mean- and varianace- standardized age at diagnosis
+pt.data$dx.age.std <- scale(pt.data$dx.age)
 
 #covariate matrix for predictors of PGG
-#### NEED TO ADD MAT FOR RANDOM EFFECTS MODEL
-pt.data$dx.age.std <- scale(pt.data$dx.age)  ## 04/13/22 Zitong
-modmat_cancer <- as.matrix(cbind(pt.data$dx.age.std))
+modmat_cancer <- as.matrix(cbind(pt.data$dx.age.std, pt.data$lr.vol))
 npred_cancer<-dim(modmat_cancer)[2]
 
 
 
-### 2. Format PSA data for JAGS run 
+### 2. Format PSA data for JAGS run ------------------
 #number of observations
-(nobs_psa <- dim(psa.data)[1])
+nobs_psa <- dim(psa.data)[1]
 
 #observed PSA
 log_psa_data <- psa.data$log.psa
@@ -111,23 +123,12 @@ sum(is.na(psa_patient_index_map))
 length(unique(psa_patient_index_map))
 
 #covariate matrix for random effects
-# psa.data$years <- psa.data$days/365.25
-# Z.data <- as.matrix(cbind(rep(1,n_obs_psa), psa.data$years))
-# d.Z <- dim(Z.data)[2]
-# data.check(condition=as.logical(sum(is.na(Z.data))==0), message="Missing time since dx in PSA data. Email Yates; she will check orginal script.")
-# summary(psa.data$days)
 modmat_ranef_psa <- as.matrix(cbind(rep(1,nobs_psa), psa.data$time.since.dx))
 npred_ranef_psa <- dim(modmat_ranef_psa)[2]
 data.check(condition=as.logical(sum(is.na(modmat_ranef_psa))==0), message="Missing time since dx in PSA data. Email Yates; she will check orginal script.")
 
 
 #covariate matrix for fixed effects
-# psa.data$dx.age.std<-scale(psa.data$dx.age.std)
-# X.data <- as.matrix(cbind(psa.data$vol.std, psa.data$dx.age.std))
-# d.X <- dim(X.data)[2]
-# data.check(condition=as.logical(sum(is.na(X.data))==0), message="Missing volumes in PSA data. Email Yates; she will check orginal script.")
-# summary(psa.data$vol.std)
-# summary(psa.data$dx.age.std)
 psa.data$dx.age.std<-vector(length=nobs_psa)
 for(i in 1:npat){
   psa.data$dx.age.std[psa.data$subj==pt.data$subj[i]]<-pt.data$dx.age.std[i]}
@@ -148,8 +149,9 @@ index.time<-c(1:2)[names(var_vec)=="time.since.dx"]
 var_vec <- c(var_vec[index.intercept], var_vec[index.time])
 
 
-### 3. Format biopsy result (reclassification) data for JAGS run 
-#number of biopsies with GS outcome
+### 5. Format biopsy result (reclassification) data for JAGS run -------------
+#### IF NPC outcome was included in the model, would want to limir biopsy grade results to biopsies with cancer found
+#make sure there are no missing biopsy grade outcomes
 bx.full$pgg[bx.full$npc==0 & !is.na(bx.full$npc)]<-1
 
 #subset data to intervals with biopsy occurring
@@ -157,21 +159,24 @@ pgg.data <- bx.full[bx.full$bx.here==1 & !is.na(bx.full$bx.here)
                     & !is.na(bx.full$pgg)
                     &  bx.full$time.int>0,]
 
+#number of biopsies with GS outcome
 n_pgg <- dim(pgg.data)[1]
 
 #GS outcomes for biopsies
 PGG <- as.numeric(pgg.data$pgg)
-table(PGG)
 data.check(condition=as.logical(sum(is.na(PGG))==0), message="Missing biopsy results data. Email Yates; she will check orginal script.")
 
 #unique subj identifier
 subj_pgg <- pgg.data$subj2
 
 #covariate matrix
-#covariate matrix
 V.PGG.data <- as.matrix(cbind(pgg.data$bx.dt.num, pgg.data$ncs, pgg.data$mri, pgg.data$std.vol))
 d.V.PGG <- dim(V.PGG.data)[2]
 
+pgg_pirads_data1 <- as.matrix(ifelse(is.na(pgg.data$pirads),0, pgg.data$pirads))
+
+
+#for intervals with second biopsies
 pgg.data2 <- pgg.data[!is.na(pgg.data$bx.time.min),]
 #& !is.na(pgg.data$npc.min) & pgg.data$npc.min>=0,]
 n_pgg2 <- dim(pgg.data2)[1]
@@ -179,6 +184,7 @@ PGG2 <- rep(1,n_pgg2)
 subj_pgg2 <- pgg.data2$subj2
 V.PGG2.data <- as.matrix(cbind(pgg.data2$bx.dt.num.min, pgg.data2$ncs.min,
                                pgg.data2$mri.min, pgg.data2$std.vol))
+pgg_pirads_data2 <- as.matrix(ifelse(is.na(pgg.data2$pirads.min),0, pgg.data2$pirads.min))
 
 #combine all outcomes
 npat_pgg <- n_pgg + n_pgg2
@@ -187,6 +193,9 @@ pgg_patient_index_map <- c(subj_pgg, subj_pgg2)
 data.check(condition=as.logical(sum(is.na(pgg_data))==0), message="Missing biopsy results data. Email Yates; she will check orginal script.")
 
 V.PGG.data <- rbind(V.PGG.data, V.PGG2.data)
+pgg_pirads_data <- c(rbind(pgg_pirads_data1, pgg_pirads_data2))
+pgg_pirads_data_m2 <- c(rbind(pgg_pirads_data1-2, pgg_pirads_data2-2))
+
 #natural splines for continuous variables
 bx.date.knots <- attr(ns(V.PGG.data[,1], 3), "knots")
 bx.date.bknots <- attr(ns(V.PGG.data[,1], 3), "Boundary.knots")
@@ -201,18 +210,33 @@ data.check(condition=as.logical(sum(is.na(V.PGG.data))==0), message="Missing bio
 
 
 ### 6. Format MRI data for JAGS run -------------
+## load MRI data: this is done in section7 of data-load-check-and-shaping.R
 ## formate MRI data for outcome model
-my.max <- function(x) ifelse( !all(is.na(x)), max(x, na.rm=T), NA)
-mri_data <- mri.data.new %>% 
-  dplyr::select(clinical_ptnum, pirads) %>% 
-  group_by(clinical_ptnum) %>% 
-  summarize(pirads_max = my.max(pirads))
-mri_data$id<-mri_data$clinical_ptnum
-mri_data$subj<-rep(0,dim(mri_data)[1])
-for(i in 1:npat){
-  mri_data$subj[mri_data$id==pt.data$id[i]] <- i}
+my.max <- function(x) ifelse(!all(is.na(x)), max(x, na.rm=T), NA)
+if(grepl("6.15", name.of.mri.data)){
+  mri_data <- mri.data.new %>% 
+    dplyr::select(clinical_ptnum, pirads) %>% 
+    group_by(clinical_ptnum) %>% 
+    summarize(pirads_max = my.max(pirads))
+  mri_data$id<-mri_data$clinical_ptnum
+}else{
+  mri_data <- mri.data.new %>% 
+    dplyr::select(Clinical_Ptnum, mri_id, pirads_update) %>% 
+    group_by(Clinical_Ptnum,mri_id) %>% 
+    summarize(pirads_max = my.max(pirads_update))
+  mri_data$id<-mri_data$Clinical_Ptnum
+}
+
+mri_data$subj<-vector(length=dim(mri_data)[1])
+for(i in 1:n){mri_data$subj[mri_data$id==pt.data$id[i]] <- i}
+
+mri_data$subj2<-vector(length=dim(mri_data)[1])
+for(i in 1:n){mri_data$subj2[mri_data$subj==i]<-pt.data$subj2[pt.data$subj==i]}
+
+if(K==1 & sum(mri_data$subj2 != mri_data$subj)>0){
+  warning(paste0(sum(mri_data$subj2 != mri_data$subj), " patients indexing (subj) incorrectly for non-cv run in mri_data, check script"))}
+
 mri_data <- mri_data[mri_data$subj != 0,]
-n_mri <- dim(mri_data)[1]
 
 mri_data_c <- mri_data[complete.cases(mri_data$pirads_max),]
 tmp_pirads_data <- mri_data_c$pirads_max
@@ -221,20 +245,4 @@ pirads_data <- ifelse(tmp_pirads_data %in% c(1, 2), 1,
 pirads_patient_index_map <- mri_data_c$subj
 npat_pirads <- dim(mri_data_c)[1]
 npred_pirads<-0
-
-nobs_pirads <- dim(mri_data_c)[1]
-
-## formate MRI data for moderator model (add to pgg regressions)
-tmp_dt <- pt.data %>% 
-  dplyr::select(subj,id, clinical_PTnum, true.pgg, bx.pgg) %>% 
-  left_join(mri_data, by = c("clinical_PTnum" = "clinical_ptnum", "id" = "id", "subj" = "subj"))
-ind_mri_data <- ifelse(is.na(tmp_dt$pirads_max), 0, 1)
-
-pgg_pirads_data <- ifelse(is.na(tmp_dt$pirads_max), 0, tmp_dt$pirads_max)
-pgg_pirads_data_m2 <- ifelse(pgg_pirads_data-2 <=0, 0, pgg_pirads_data-2)
-
-
-
-
-
 
