3cbf4067e7e2907b5af39b77cdeaa64035e24bfd
Zitong Wang <zwang238@jh.edu>
2022-12-12 19:45:04 -0500

    update .sh

--- cleaned_code/submit_cv.sh
+++ cleaned_code/submit_cv.sh
@@ -1,11 +1,13 @@
 node_name='shared.q@compute-*'
 mem_gb_free=10
 n_core=1
+workdir="/users/zwang3/PAS_11722"
+mri_role="both"
 
-for ((to_mask=1; to_mask<=${1:-1}; to_mask++)) do
-    job_name="cv${to_mask}"
-    mkdir -p "${HOME}/generated-files-cv" # Make a directory if non-existent
-    log_file_name="${HOME}/generated-files-cv/${job_name}_qsub_log.txt"
+for ((to_mask=1;to_mask<=${1:-1};to_mask++)) do
+    job_name="cv${to_mask}_${mri_role}"
+    #mkdir -p "${workdir}/generated-files-sh" # Make a directory if non-existent
+    log_file_name="${workdir}/logs/${job_name}_qsub_log.txt"
     qsub \
       -q "${node_name}" `# If you need a specific node` \
       -l mem_free="${mem_gb_free}G",h_vmem="${mem_gb_free}G" `# Specify memory requirement` \
@@ -14,6 +16,8 @@ for ((to_mask=1; to_mask<=${1:-1}; to_mask++)) do
       -o $log_file_name `# Direct output messages` \
       -e $log_file_name `# Direct errors` \
       -m e -M zwang238@jh.edu `# Send an email when the job completes or aborts` \
-      -v K=${1:-1}, to_mask=${to_mask}, job_name=${job_name} `# Assign variables to be passed to the bash script` \
-      submit_single_cv.sh
+      -v K=${1:-1},to_mask=${to_mask},mri_role=${mri_role},workdir=${workdir},job_name=${job_name} `# Assign variables to be passed to the bash script` \
+      run_single_simulation.sh
 done
+#sh submit_simulation.sh: submit for non-cv (K = 1)
+#sh submit_simulation.sh K: submit for K fold cv 
--- cleaned_code/submit_single_cv.sh
+++ cleaned_code/submit_single_cv.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-rscript_dir=" /users/zwang3/PAS_clean/cleaned_code"
-rscript_log_file_name="${HOME}/generated-files-cv/${job_name}_rscript_log.txt"
-mkdir -p "${HOME}/generated-files-cv" # Make a directory if non-existent
+workdir="/users/zwang3/PAS_11722"
+rscript_log_file_name="${workdir}/logs/${job_name}_rscript_log.txt"
+#mkdir -p "${workdir}/generated-files-sh" # Make a directory if non-existent
 touch "$rscript_log_file_name" # Create the file if non-existent
 module load conda_R
 echo "Starting ${job_name}..."
-Rscript "${rscript_dir}/cv-to-run-single.R" 
+Rscript "${workdir}/code/run-single.R" $K $to_mask $mri_role $workdir &> $rscript_log_file_name
