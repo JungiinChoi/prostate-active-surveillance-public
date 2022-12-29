3f6b0d99702e1bb5f15dfc9541a05d3a83f7cf39
Zitong Wang <zwang238@jh.edu>
2022-12-29 14:17:56 -0500

    add processing file for bx data

--- code/submit_single_cv.sh
+++ code/submit_single_cv.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-workdir="/users/jchoi/prostate-active-surveillance-vDaan"
+workdir="/users/zwang3/PAS_11722"
 rscript_log_file_name="${workdir}/logs/${job_name}_rscript_log.txt"
 #mkdir -p "${workdir}/generated-files-sh" # Make a directory if non-existent
 touch "$rscript_log_file_name" # Create the file if non-existent
 module load conda_R
 echo "Starting ${job_name}..."
-Rscript "${workdir}/cleaned_code/cv-to-run-single.R" $K $to_mask $mri_role $workdir &> $rscript_log_file_name
+Rscript "${workdir}/code/run-single.R" $K $to_mask $mri_role $workdir &> $rscript_log_file_name
